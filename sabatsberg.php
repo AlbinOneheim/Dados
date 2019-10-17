e8806717f696fdf402c628e1dbd2b9d376cccdeb
elev <elev@8FY06H2.learnet.se>
2019-10-17 12:19:32 +0200

    test

--- sabatsberg.php
+++ sabatsberg.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kontakt</title>
+    <title>Sabatsberg</title>
     <link rel="stylesheet" href="style.css">
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <script src="./javascript.js" defer></script>
@@ -12,9 +12,17 @@
     <header>
         <a href="./startsida.html"><img class="loga" src="./bilder/dadoslogo.png" alt=""></a>
         <div class="mail">
-            <i class='far fa-envelope'></i>
-            <p class="p-kontakt">Kontakt</p>
-        </div>   
+            <a href="./kontakt.html">
+                <i class='far fa-envelope'></i>
+                <p class="p-kontakt">Kontakt</p>
+            </a>
+        </div>  
     </header>
+    <main>
+        <h2>Lunchmeny</h2>
+        <h3>Sabatsbergs sjukhus</h3>
+        <h4 class="veckonummer">Vecka 42</h4>
+
+    </main>
 </body>
 </html>
